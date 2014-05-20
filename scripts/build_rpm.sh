1dd20b8feebef5f362e107896ced77576d2d6869
Derek Higgins <derekh@redhat.com>
2014-05-20 14:11:15 +0000

    More generic build script
    Can now build python-*.spec rpm's.

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -6,19 +6,19 @@ yum install -y --nogpg python-pip
 sleep 3
 cd /data/$1
 python setup.py sdist
-TARBALL=$(ls dist/* | grep -Eo "$1.*") 
+TARBALL=$(ls dist)
 mv dist/$TARBALL ~/rpmbuild/SOURCES/
 
 cd /data/$1_spec
 cp * ~/rpmbuild/SOURCES/
-cp openstack-$1.spec ~/rpmbuild/SPECS/
+cp *.spec ~/rpmbuild/SPECS/
 cd ~/rpmbuild/SPECS/
 
 VERSION=${TARBALL%%.tar*}
 VERSION=${VERSION//*-}
 
-sed -i -e "s/Version:.*/Version: $VERSION/g" openstack-$1.spec
-sed -i -e "s/Source0:.*/Source0: $TARBALL/g" openstack-$1.spec
-yum-builddep -y openstack-$1.spec
-rpmbuild -ba openstack-$1.spec &> $2/rpmbuild.log
+sed -i -e "s/Version:.*/Version: $VERSION/g" *.spec
+sed -i -e "s/Source0:.*/Source0: $TARBALL/g" *.spec
+yum-builddep -y *.spec
+rpmbuild -ba *.spec
 find /rpmbuild/RPMS /rpmbuild/SRPMS -type f | xargs cp -t $2
