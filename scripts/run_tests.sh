d5eab61fd96d3c057f8de29c8d8e0ad5d5ca6451
Derek Higgins <derekh@redhat.com>
2015-02-11 00:46:25 +0000

    WIP: Add test script
    Change-Id: Ia7113040c7f02fb0258963f2c3fdf5b9db0215b5

---
+++ scripts/run_tests.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+set -eux
+
+# Simple CI test to sanitiy test commits
+
+# Make sure docker is running
+sudo systemctl start docker
+
+# Display the current commit
+git log -1
+
+# Set up rdoinfo
+# TODO(derekh): Remove this once we're using rdopkg
+git clone https://github.com/redhat-openstack/rdoinfo.git --depth 1
+export PYTHONPATH=rdoinfo
+
+# Run unit tests
+tox -epy27
+
+# Run pep8 tests
+tox -epep8
+
+# Create a build image
+./scripts/create_build_image.sh
+
+# And Run  delorean against a project
+. .tox/py27/bin/activate
+delorean --config-file projects.ini --info-file ./rdoinfo/rdo.yml --head-only --package-name python-keystoneclient --dev
