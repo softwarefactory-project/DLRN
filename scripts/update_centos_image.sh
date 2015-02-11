ae18cf25e87871ae9ac806ac5f1797ca841523f5
Alan Pevec <alan.pevec@redhat.com>
2015-02-11 23:44:55 +0100

    CentOS 7 has epel-release
    in its by default enabled Extras repo
    
    Change-Id: Id8fc016871fec7676fc4b733f45028e05eed69bc

--- scripts/update_centos_image.sh
+++ scripts/update_centos_image.sh
@@ -2,7 +2,7 @@
 
 yum clean all
 yum update -y --nogpg
-rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+yum install -y --nogpg epel-release
 yum install -y --nogpg rpm-build git python-setuptools yum-utils python2-devel intltool make python-pip gcc yum-plugin-priorities
 # temp deps
 yum install -y --nogpg python-sqlalchemy python-webob python-eventlet ghostscript graphviz python-sphinx
