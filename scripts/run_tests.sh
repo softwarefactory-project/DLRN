38f2753e2243c4c3e0ffb9aa6b9e64870b244981
Alan Pevec <alan.pevec@redhat.com>
2015-02-20 00:17:37 +0100

    Rename distros to distro parameter in projects.ini
    To make it clear parameter is NOT a list but a single value.
    Also rename RPMizm "spec" to neutral "distro" everywhere
    to allow future support of non-RPM distros.
    
    Remove TODO Add support for multiple distros - this can be achieved
    by running multiple Delorean instances with separate projects.ini
    
    Also make pep8 happy by splitting long lines.
    
    Change-Id: I6df971b277e2d8d8d901be629cf5d3398fd27fd9

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -29,15 +29,15 @@ set -u
 PROJECT_TO_BUILD=python-keystoneclient
 PROJECT_TO_BUILD_MAPPED=$(./scripts/map-project-name $PROJECT_TO_BUILD)
 
-# If this is a CI run for one of the spec files then we pre download it
+# If this is a CI run for one of the distro repositories then we pre download it
 # into the data directory, delorean wont change it because we are using --dev
 if [ -n "$GERRIT_PROJECT" ] && [ "$GERRIT_PROJECT" != "openstack-packages/delorean" ] ; then
     mkdir -p data/repos
     PROJECT_TO_BUILD=${GERRIT_PROJECT#*/}
     PROJECT_TO_BUILD_MAPPED=$(./scripts/map-project-name $PROJECT_TO_BUILD)
-    PROJECT_SPEC_DIR=${PROJECT_TO_BUILD_MAPPED}_spec
-    git clone https://review.gerrithub.io/"$GERRIT_PROJECT" data/$PROJECT_SPEC_DIR
-    pushd data/$PROJECT_SPEC_DIR
+    PROJECT_DISTRO_DIR=${PROJECT_TO_BUILD_MAPPED}_distro
+    git clone https://review.gerrithub.io/"$GERRIT_PROJECT" data/$PROJECT_DISTRO_DIR
+    pushd data/$PROJECT_DISTRO_DIR
     git fetch https://review.gerrithub.io/$GERRIT_PROJECT $GERRIT_REFSPEC && git checkout FETCH_HEAD
     popd
 fi
@@ -53,7 +53,7 @@ cp -r data/repos logs/fedora
 sed -i -e 's%target=.*%target=centos%' projects.ini
 sed -i -e 's%baseurl=.*%baseurl=http://104.130.230.24/centos70%' projects.ini
 
-# And run delorean again, for the moment we mask failures i.e. report only until we're sure all the specs run
+# And run delorean again
 delorean --config-file projects.ini --head-only --package-name $PROJECT_TO_BUILD_MAPPED --dev
 
 # Copy files to be archived
