ef94c84e0785fd08e273a921bc231d513aa2b9f5
Javier Pena <jpena@redhat.com>
2015-06-17 16:28:13 +0200

    Do not use sudo when removing repo dir
    There is no guarantee that the user running delorean will have full
    sudo rights without password, and the user should have permissions
    over that directory anyway.
    
    Change-Id: I5edf8a0fdce450567f918894d1228cea16cbdb8d

--- delorean/shell.py
+++ delorean/shell.py
@@ -338,7 +338,7 @@ def refreshrepo(url, path, branch="master", local=False):
             # unusable, to avoid a looping error just remove it so it will be
             # recloned.
             logger.error("Error fetching into %s, deleting." % (path))
-            sh.sudo("rm", "-rf", path)
+            sh.rm("-rf", path)
             raise
     git.checkout(branch)
     git.reset("--hard", "origin/%s" % branch)
