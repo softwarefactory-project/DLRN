688931cb388721bbd5b3a77d0d2ab70457c3e51d
Frederic Lepied <frederic.lepied@redhat.com>
2015-08-24 19:16:18 +0000

    Use mock instead of a custom docker machinery to build packages
    Change-Id: I068c2f427ef5dc611665fc55b3f3956247bfb113

--- scripts/build_rpm_wrapper.sh
+++ scripts/build_rpm_wrapper.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -xe
 
+DIR=$(realpath $(dirname $0))
+
 if [ "$1" != "openstack-puppet-modules" ] ; then
-    /scripts/build_rpm.sh $@ &> $2/rpmbuild.log
+    $DIR/build_rpm.sh "$@" &> $2/rpmbuild.log
 else
     # Special case of the puppet modules as they don't
     # come from a single repo
-    /scripts/build_rpm_opm.sh $@ &> $2/rpmbuild.log
+    $DIR/build_rpm_opm.sh "$@" &> $2/rpmbuild.log
 fi
