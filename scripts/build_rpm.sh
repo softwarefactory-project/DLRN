4e96d0e48a2b9d0774d9d933ccccc50ec275952b
Frederic Lepied <frederic.lepied@redhat.com>
2015-08-24 19:16:18 +0000

    Use mock instead of a custom docker machinery to build packages
    Change-Id: I068c2f427ef5dc611665fc55b3f3956247bfb113

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -1,42 +1,8 @@
 #!/bin/bash -xe
 
+set -o pipefail
+
 source $(dirname $0)/common-functions
-
-PROJECT_NAME=$1
-OUTPUT_DIRECTORY=$2
-USER_ID=$3 # chown resulting files to this UID
-GROUP_ID=$4 # chown resulting files to this GUID
-BASEURL=$5
-
-mkdir -p ~/rpmbuild/SOURCES ~/rpmbuild/SPECS $OUTPUT_DIRECTORY
-
-trap finalize EXIT
-
-# Set up the repo for dependencies
-curl ${BASEURL}/delorean-deps.repo > /etc/yum.repos.d/delorean-deps.repo
-
-# Install a recent version of python-pbr
-if ! rpm -q python-pbr ; then
-    yum install -y python-pbr python3-pbr
-fi
-
-# install rdo-rpm-macros as a quick fix for el7 buildroot
-if [[ ${BASEURL} == *"centos"* ]]
-then
-    yum install -y rdo-rpm-macros
-fi
-
-# install latest build tools updates from RDO repo
-yum install -y --nogpg python-pip python-setuptools
-
-# If in dev mode the user might not be building all of the packages, so we need
-# to add the current upstream repository in order to have access to current dependencies
-if [ "$DELOREAN_DEV" == "1" ] ; then
-    curl ${BASEURL}/current/delorean.repo > /etc/yum.repos.d/public_current.repo
-fi
-
-cd /data/$PROJECT_NAME
-rm -f dist/*
 
 for FILE in {test-,}requirements.txt
 do
@@ -92,30 +58,27 @@ if [[ "$PROJECT_NAME" =~  ^(diskimage-builder|tripleo-heat-templates|tripleo-ima
    fi
 fi
 
-mv dist/$TARBALL ~/rpmbuild/SOURCES/
+mv dist/$TARBALL ${TOP_DIR}/SOURCES/
 
-cd /data/${PROJECT_NAME}_distro
-cp * ~/rpmbuild/SOURCES/
-cp *.spec ~/rpmbuild/SPECS/
+cd ${DATA_DIR}/${PROJECT_NAME}_distro
+cp * ${TOP_DIR}/SOURCES/
+cp *.spec ${TOP_DIR}/SPECS/
 # Generate a diff of this distro repo when compared to Fedora Rawhide
 if git fetch http://pkgs.fedoraproject.org/git/$PROJECT_NAME master ; then
     git diff HEAD..FETCH_HEAD > $OUTPUT_DIRECTORY/distro_delta.diff
 fi
-cd ~/rpmbuild/SPECS/
+cd ${TOP_DIR}/SPECS/
 
-# Add the mostcurrent repo, we may have dependencies in it
-if [ -e /data/repos/current/repodata ] ; then
-    echo -e '[current]\nname=current\nbaseurl=file:///data/repos/current\nenabled=1\ngpgcheck=0\npriority=1' > /etc/yum.repos.d/current.repo
-fi
-
+sed -i -e "1i%define upstream_version $UPSTREAMVERSION\\" *.spec
 sed -i -e "s/UPSTREAMVERSION/$UPSTREAMVERSION/g" *.spec
 VERSION=${VERSION/-/.}
 sed -i -e "s/Version:.*/Version: $VERSION/g" *.spec
 sed -i -e "s/Release:.*/Release: $RELEASE%{?dist}/g" *.spec
 sed -i -e "s/Source0:.*/Source0: $TARBALL/g" *.spec
 cat *.spec
-yum-builddep --disablerepo="*source" -y *.spec
-rpmbuild -ba *.spec  --define="upstream_version $UPSTREAMVERSION"
-find ~/rpmbuild/RPMS ~/rpmbuild/SRPMS -type f | xargs cp -t $OUTPUT_DIRECTORY
+rpmbuild --define="_topdir ${TOP_DIR}" -bs ${TOP_DIR}/SPECS/*.spec
+mock -v -r $(dirname $0)/delorean.cfg --postinstall --resultdir $OUTPUT_DIRECTORY --rebuild ${TOP_DIR}/SRPMS/*.src.rpm 2>&1 | tee $OUTPUT_DIRECTORY/mock.log
 
-yum install -y --nogpg $(find $OUTPUT_DIRECTORY -type f -name "*rpm" | grep -v src.rpm) && touch $OUTPUT_DIRECTORY/installed || true
+if ! grep -F 'WARNING: Failed install built packages' $OUTPUT_DIRECTORY/mock.log; then
+    touch $OUTPUT_DIRECTORY/installed
+fi
