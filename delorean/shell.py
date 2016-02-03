86aa3606bfb37f1147eb39c65125b27af9ead29f
Javier Pena <jpena@redhat.com>
2016-02-03 14:35:35 +0100

    Add compatibility with earlier versions of sh
    python-sh prior to version 1.10 does not properly support using a
    program name that can be shadowed by a name used internally (see
    https://github.com/amoffat/sh/blob/master/CHANGELOG.md#110---123014).
    
    As a workaround, let's detect the sh version and use the right command
    for each case.
    
    Change-Id: I9d23ef38d6b714c7abbe3f266ff6fd735ffee008

--- delorean/shell.py
+++ delorean/shell.py
@@ -30,6 +30,7 @@ from prettytable import PrettyTable
 import sh
 from six.moves import configparser
 from six.moves.urllib import parse
+from pbr.version import SemanticVersion
 
 import rdopkg.utils.log
 rdopkg.utils.log.set_colors('no')
@@ -463,7 +464,12 @@ def build(cp, package_info, commit, env_vars, dev_mode, use_public):
                     os.path.join(datadir, yumrepodir),
                     datadir, baseurl])
     try:
-        sh.env(run_cmd)
+        sh_version = SemanticVersion.from_pip_string(sh.__version__)
+        min_sh_version = SemanticVersion.from_pip_string('1.09')
+        if sh_version > min_sh_version:
+            sh.env(run_cmd)
+        else:
+            sh.env_(run_cmd)
     except Exception as e:
         logger.error('cmd failed. See logs at: %s/%s/' % (datadir,
                                                           yumrepodir))
