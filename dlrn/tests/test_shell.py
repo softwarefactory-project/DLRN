eeb4a0143a3664d4794cc4ab7ddb42d850e1e3ef
Javier Pena <jpena@redhat.com>
2016-04-15 16:58:36 +0200

    Follow-up on purge patch
    Some fixes were still missing:
    - Add documentation
    - Add tests
    - Fix corner cases where repos where deleted but they shouldn't
    - Fix corner cases where repos where not deleted but they should
    
    Change-Id: I938e87689f13fc5648a64ea85a0c1109b70b3d8d

--- dlrn/tests/test_shell.py
+++ dlrn/tests/test_shell.py
@@ -13,9 +13,22 @@
 # under the License.
 import mock
 import sh
+import sys
 
-from dlrn import shell
+from datetime import datetime
+
 from dlrn.tests import base
+
+from dlrn import db
+from dlrn import shell
+from dlrn import utils
+
+
+class TestsWithData(base.TestCase):
+    def setUp(self):
+        super(TestsWithData, self).setUp()
+        self.session = db.getSession(new=True)
+        utils.loadYAML(self.session, './dlrn/tests/samples/commits_1.yaml')
 
 
 @mock.patch.object(sh.Command, '__call__', autospec=True)
@@ -46,3 +59,46 @@ class TestRefreshRepo(base.TestCase):
                     mock.call(sh.git.reset, '--hard', 'origin/branch'),
                     mock.call(sh.git.log, '--pretty=format:%H %ct', '-1')]
         self.assertEqual(sh_mock.call_args_list, expected)
+
+
+def mocked_session(url):
+    session = db.getSession(new=True)
+    utils.loadYAML(session, './dlrn/tests/samples/commits_1.yaml')
+    return session
+
+
+def mocked_listdir(path):
+    return ['file1.rpm', 'file2.rpm', 'file3.rpm']
+
+
+def mocked_islink(path):
+    return False
+
+expected_repos = [
+    './data/repos/6a/bf/6abf557aa1d8fff0aa21f8eba6cd18302c2c86ff_0b1ce934',
+    './data/repos/ae/9d/ae9d27e5100f002f55ad6eb2b252a0aa5f16a336_024e24f0',
+    './data/repos/45/95/459549c9ab7fef91b2dc8986bc0643bb2f6ec0c8_885e8077',
+    './data/repos/e4/f7/e4f71ada86ee4a42287cf401b77d48c9f98ca5aa_354991d9',
+    './data/repos/80/f6/80f6969520b526b01c210b1c680565bef8a1f8ad_024e24f0',
+    './data/repos/12/ed/12edde8a53c7299105bb8297c6f31d3a458615ab_354991d9',
+    './data/repos/17/23/17234e9ab9dfab4cf5600f67f1d24db5064f1025_024e24f0']
+
+
+@mock.patch('os.path.islink', side_effect=mocked_islink)
+@mock.patch('os.listdir', side_effect=mocked_listdir)
+@mock.patch('dlrn.shell.getSession', side_effect=mocked_session)
+@mock.patch('shutil.rmtree')
+class TestPurge(base.TestCase):
+
+    @mock.patch('dlrn.shell.datetime')
+    def test_purge(self, dt_mock, sh_mock, db_mock, lst_mock, il_mock):
+        testargs = ["dlrn-purge", "--config-file",
+                    "projects.ini", "--older-than",
+                    "1", "-y"]
+        dt_mock.now.return_value = datetime(2015, 10, 1, 14, 20)
+        with mock.patch.object(sys, 'argv', testargs):
+            shell.purge()
+            expected = []
+            for repo in expected_repos:
+                expected.append(mock.call(repo))
+            self.assertEqual(sh_mock.call_args_list, expected)
