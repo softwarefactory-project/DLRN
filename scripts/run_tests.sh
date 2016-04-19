6bf05fa09e4708000a6a5602f6d0168221699740
David Moreau-Simard <dms@redhat.com>
2016-04-19 11:18:42 -0400

    Resolve unit test issues
    - Pass the $TERM env variable to tox
      This is required for the test "dlrn.tests.test_shell"
      which is required or fails with an error.
    
    - Add setuptools to test-requirements
      mock requires setuptools>17.1 which is not available in
      CentOS base OS repositories.
    
    - Refactor and simplify run_tests.sh
      - Take out unit tests and pep8 (they will have their own
        jobs)
      - Remove code that was required to support fedora and
        centos simultaneously (support centos exclusively)
      - Remove code that was required to support gerrit and
        zuul simultaneously (support zuul or locally)
      - Lots of DRY streamlining
      - General rework to adapt to review.rdoproject
        environment
    
    Change-Id: I860046cc6ff8f2271d46ae58b0beae1f406bd60e

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,129 +1,80 @@
 #!/bin/bash
 set -ex
 
-# Simple CI test to sanity test commits
-
-RDOINFO="$1"
+# Simple script to test that delorean works either locally or in a zuul environment
+GIT_BASE_URL="https://review.rdoproject.org/r/p"
+RDOINFO="${1:-$GIT_BASE_URL/rdoinfo}"
 
 # Display the current commit
 git log -1
 
-set +u
-if [ -z "${GERRIT_PROJECT-}" -a -z "${ZUUL_PROJECT-}" -o "$GERRIT_PROJECT" = "openstack-packages/delorean" -o "$ZUUL_PROJECT" = "openstack-packages/delorean" ]; then
-    # Run unit tests
-    tox -epy27
+# Run bash unit tests
+./scripts/run_sh_tests.sh
 
-    # Run pep8 tests
-    tox -epep8
-
-    # Run bash unit tests
-    ./scripts/run_sh_tests.sh
-else
-    # Only prepare virtualenv
-    tox -epy27 --notest
-fi
-
-# Use the env setup by tox
+# Setup virtualenv with tox and use it
+tox -epy27 --notest
 . .tox/py27/bin/activate
-set -u
 
-# test that the mock command is present
-type -p mock
+# Default project to build
+PROJECT_DISTRO="openstack/glanceclient-distgit"
+PROJECT_DISTRO_BRANCH="rpm-master"
 
-# Build this if not building a specific project
-PROJECT_TO_BUILD=python-glanceclient
+# If we're actually building for a Zuul project, build that unless it's DLRN
+[[ ! -z "${ZUUL_PROJECT}" && "${ZUUL_PROJECT}" != "DLRN" ]] && PROJECT_DISTRO="${ZUUL_PROJECT}"
+
+# The rdoinfo project name isn't prefixed by openstack/ or suffixed by -distgit
+PROJECT_TO_BUILD=${PROJECT_DISTRO#*/}
+PROJECT_TO_BUILD=$(sed "s/-distgit//" <<< "${PROJECT_TO_BUILD}")
+
+# Map to rdoinfo
 PROJECT_TO_BUILD_MAPPED=$(./scripts/map-project-name $PROJECT_TO_BUILD $RDOINFO)
+PROJECT_DISTRO_DIR=${PROJECT_TO_BUILD_MAPPED}_distro
 
-# If this is a CI run for one of the distro repositories then we pre download it
-# into the data directory, delorean wont change it because we are using --dev
-[ -z "${GERRIT_PROJECT-}" ] && GERRIT_PROJECT=""
-[ -z "${ZUUL_PROJECT-}" ] && ZUUL_PROJECT=""
-if [ -n "$GERRIT_PROJECT" ] && [ "$GERRIT_PROJECT" != "openstack-packages/delorean" ] ; then
-    mkdir -p data/repos
-    PROJECT_TO_BUILD=${GERRIT_PROJECT#*/}
-    PROJECT_TO_BUILD_MAPPED=$(./scripts/map-project-name $PROJECT_TO_BUILD $RDOINFO)
-    PROJECT_DISTRO_DIR=${PROJECT_TO_BUILD_MAPPED}_distro
-    # Use different cloning directory for regular and packaging repositories
-    if [ "${GERRIT_PROJECT/openstack-packages\/*/packages}" == "packages" ] ; then
-        PROJECT_CLONE_DIR=$PROJECT_DISTRO_DIR
-    else
-        PROJECT_CLONE_DIR=$PROJECT_TO_BUILD_MAPPED
-    fi
-    git clone https://$GERRIT_HOST/"$GERRIT_PROJECT" data/$PROJECT_CLONE_DIR
-    pushd data/$PROJECT_CLONE_DIR
-    git fetch https://$GERRIT_HOST/$GERRIT_PROJECT $GERRIT_REFSPEC && git checkout FETCH_HEAD
-    popd
-# If this a CI run triggered by Zuul. Use zuul-cloner to fetch the project at the right
-# revision.
-elif [ -n "$ZUUL_PROJECT" ] ; then
-    # test that the zuul-cloner command is present
-    type -p zuul-cloner
-    mkdir -p data/repos
-    PROJECT_TO_BUILD=${ZUUL_PROJECT%-distgit}
-    PROJECT_TO_BUILD=${PROJECT_TO_BUILD#*/}
-    PROJECT_TO_BUILD_MAPPED=$(./scripts/map-project-name $PROJECT_TO_BUILD $RDOINFO)
-    PROJECT_DISTRO_DIR=${PROJECT_TO_BUILD_MAPPED}_distro
-    zuul-cloner --workspace data/ $GERRIT_CLONE_URL $ZUUL_PROJECT
-    mv data/$ZUUL_PROJECT data/$PROJECT_DISTRO_DIR
+# Prepare config
+target="centos"
+baseurl="http://trunk.rdoproject.org/centos7/"
+src="master"
+branch=""
+
+# If we're testing a commit on a specific branch, make sure we're using it
+if [[ "${ZUUL_BRANCH}" =~ rpm- && "${ZUUL_BRANCH}" != "rpm-master" ]]; then
+    branch=$(sed "s/rpm-//" <<< "${ZUUL_BRANCH}")
+    baseurl="http://trunk.rdoproject.org/${branch}/centos7/"
+    src="stable/${branch}"
+    PROJECT_DISTRO_BRANCH=$ZUUL_BRANCH
 fi
 
-function update_config() {
-    # Ensures configuration is set properly according to distribution and branch
-    case "${1}" in
-        centos)
-          target="centos"
-          baseurl="http://trunk.rdoproject.org/centos7/"
-          src="master"
-          branch=""
-          ;;
-        fedora)
-          target="fedora"
-          baseurl="https://trunk.rdoproject.org/f23"
-          src="master"
-          branch=""
-          ;;
-        *)
-          target="centos"
-          baseurl="http://trunk.rdoproject.org/centos7/"
-          src="master"
-          branch=""
-          ;;
-    esac
+# Update the configuration
+sed -i "s%target=.*%target=${target}%" projects.ini
+sed -i "s%source=.*%source=${src}%" projects.ini
+sed -i "s%baseurl=.*%baseurl=${baseurl}%" projects.ini
+sed -i "s%tags=.*%tags=${branch}%" projects.ini
 
-    # If this is a commit on a specific branch, make sure we're using it
-    [ -n "${GERRIT_BRANCH-}" ] && TARGET_BRANCH=$GERRIT_BRANCH
-    [ -n "${ZUUL_BRANCH-}" ] && TARGET_BRANCH=$ZUUL_BRANCH
-    if [[ "${target}" == "centos" \
-          && "${TARGET_BRANCH}" =~ rpm- \
-          && "${TARGET_BRANCH}" != "rpm-master" ]]; then
-      branch=$(sed "s/rpm-//" <<< "${TARGET_BRANCH}")
-      baseurl="http://trunk.rdoproject.org/${branch}/centos7/"
-      src="stable/${branch}"
-    fi
-
-    # Update the configration
-    sed -i "s%target=.*%target=${DISTRO}%" projects.ini
-    sed -i "s%source=.*%source=${src}%" projects.ini
-    sed -i "s%baseurl=.*%baseurl=${baseurl}%" projects.ini
-    sed -i "s%tags=.*%tags=${branch}%" projects.ini
-}
-
-function copy_logs() {
-    mkdir -p logs
-    rsync -avzr data/repos logs/$DISTRO
-}
-
-function run_dlrn() {
-    export DISTRO="${1}"
-    update_config $DISTRO
-    # Run delorean
-    delorean --config-file projects.ini --head-only --package-name $PROJECT_TO_BUILD_MAPPED --dev
-    copy_logs
-    # Clean up mock cache, just in case there is a change for the next run
-    mock -r data/dlrn.cfg --scrub=all
-}
+# Prepare directories
+mkdir -p data/repos
+if [ -e /usr/bin/zuul-cloner ]; then
+    zuul-cloner --workspace data/ $GIT_BASE_URL $PROJECT_DISTRO --branch $PROJECT_DISTRO_BRANCH
+    mv data/$PROJECT_DISTRO data/$PROJECT_DISTRO_DIR
+else
+    # We're outside the gate, just do a regular git clone
+    pushd data/
+    # rm -rf first for idempotency
+    rm -rf $PROJECT_DISTRO_DIR
+    git clone "${GIT_BASE_URL}/${PROJECT_DISTRO}" $PROJECT_DISTRO_DIR
+    cd $PROJECT_DISTRO_DIR
+    git checkout $PROJECT_DISTRO_BRANCH
+    popd
+fi
 
 # If the commands below throws an error we still want the logs
+function copy_logs() {
+    mkdir -p logs
+    rsync -avzr data/repos logs/centos
+}
 trap copy_logs ERR EXIT
 
-run_dlrn centos
+# Run delorean
+delorean --config-file projects.ini --head-only --package-name $PROJECT_TO_BUILD_MAPPED --dev
+copy_logs
+# Clean up mock cache, just in case there is a change for the next run
+mock -r data/dlrn.cfg --scrub=all
