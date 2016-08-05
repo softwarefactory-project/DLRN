b31742023e7f144b7e27d3fa0fb65d3639e58520
Javier Pena <jpena@redhat.com>
2016-08-05 18:10:11 +0200

    Parallel mock builds in DLRN
    DLRN has always been a sequential tool, which is fine in general
    but causes performance issues when chasing master for all OpenStack
    projects.
    
    This patch implements a pool of worker processes for mock builds. It
    splits the build phase in two:
    
    1- Create mock config, build RPM package
    2- Get package build results, create repo, update db and rsync if needed
    
    Part 1 is executed by the worker processes, while part 2 is run
    sequentially by the main process. This ensures in-order repo updates.
    
    A new config option (workers) is added to projects.ini, with a
    default value of 1. This still means a performance improvement, as
    the repo for a commit will be created while the following commit
    is processed.
    
    Change-Id: Ibd5c781c7ec55f427e22dd447dcc9b78d5428d5f

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -77,4 +77,4 @@ trap copy_logs ERR EXIT
 dlrn --config-file projects.ini --head-only --package-name $PROJECT_TO_BUILD_MAPPED --dev
 copy_logs
 # Clean up mock cache, just in case there is a change for the next run
-mock -r data/dlrn.cfg --scrub=all
+mock -r data/dlrn-1.cfg --scrub=all
