691e5f9ec0512b8816199513f24b0f955a83b669
Javier Pena <jpena@redhat.com>
2016-09-19 18:40:45 +0200

    Add functionality to import commits built by another instance
    We want to enable a master-worker architecture, where a central
    instance aggregates builds made by multiple, possibly short-lived
    instances.
    
    To do so, this patch adds the following:
    
    - During the build, a new file is created in the generated repo,
      called commit.yaml. It includes all metadata from the commit that
      was just built.
    
    - A new command, dlrn-remote, is added. This command is meant to be
      executed on the "master" instance, and it will download the
      commit.yaml file, parse it, then fetch the generated repo and use
      it as if it was built locally (create repo with existing rpms, add
      info to database, etc).
    
    To test it, we will need two instances. On the worker instance, run:
    
    $ dlrn --config-file projects.ini --package-name xyz --use-public
    
    It is important *not* to use --dev, as some metadata (distgit) is lost.
    Then, on the main instance, run:
    
    $ dlrn --config-file projects.ini \
      --repo-url http://worker.ip/<repo hash>/
    
    Change-Id: I071a008f603985247563b05717e4f71653da3770

--- dlrn/shell.py
+++ dlrn/shell.py
@@ -46,6 +46,7 @@ from dlrn.rsync import sync_repo
 from dlrn.utils import dumpshas2file
 from dlrn.utils import import_object
 from dlrn.utils import isknownerror
+from dlrn.utils import saveYAML_commit
 from dlrn.utils import timesretried
 from dlrn import version
 
@@ -326,6 +327,9 @@ def main():
                                               head_only=options.head_only)
             if exit_value != 0:
                 exit_code = exit_value
+
+            # Export YAML file containing commit metadata
+            export_commit_yaml(status[0])
             if options.stop and exit_code != 0:
                 return exit_code
     else:
@@ -361,6 +365,9 @@ def main():
                                                   head_only=options.head_only)
                 if exit_value != 0:
                     exit_code = exit_value
+
+                # Export YAML file containing commit metadata
+                export_commit_yaml(status[0])
                 if options.stop and exit_code != 0:
                     return exit_code
             except StopIteration:
@@ -627,6 +634,15 @@ def getsourcebranch(package):
     else:
         config_options = getConfigOptions()
         return config_options.source
+
+
+def export_commit_yaml(commit):
+    config_options = getConfigOptions()
+    # Export YAML file containing commit metadata
+    datadir = os.path.realpath(config_options.datadir)
+    yumrepodir = os.path.join(datadir, "repos",
+                              commit.getshardedcommitdir())
+    saveYAML_commit(commit, os.path.join(yumrepodir, 'commit.yaml'))
 
 
 def post_build(status, packages, session):
