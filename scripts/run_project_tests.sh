8ca62d8d63ba38b8b460aed6d8d1540de48a1d0d
Javier Pena <jpena@redhat.com>
2016-09-29 17:10:41 +0200

    Use --use-public instead of --dev for run_project_tests.sh
    run_project_tests.sh is meant to be used when building commits from
    review.openstack.org. In those cases, there are no changes to the
    distgit from the current repo, so there is no need to use --dev.
    
    By not using --dev, we can make the CI jobs forward-compatible with
    the new DLRN+nodepool architecture, since the dlrn-remote patch
    (https://review.rdoproject.org/r/2498) requires having all commit
    metadata. With this, we can reuse the DLRN-rpmbuild job for the package
    build part.
    
    Change-Id: I5832912b9b60a17ca34ee90215575d6c9fe98e25

--- scripts/run_project_tests.sh
+++ scripts/run_project_tests.sh
@@ -87,7 +87,7 @@ function copy_logs() {
 trap copy_logs ERR EXIT
 
 # Run DLRN
-dlrn --config-file projects.ini --head-only --package-name $PROJECT_TO_BUILD_MAPPED --dev --local --info-repo /tmp/rdoinfo --verbose-mock
+dlrn --config-file projects.ini --head-only --package-name $PROJECT_TO_BUILD_MAPPED --use-public --local --info-repo /tmp/rdoinfo --verbose-mock
 copy_logs
 # Clean up mock cache, just in case there is a change for the next run
 mock -r data/dlrn-1.cfg --scrub=all
