aaecbe68ec97690c9aa09eb0b580575892ae2a7c
Javier Pena <jpena@redhat.com>
2016-11-11 12:26:13 +0100

    Initial DLRN API
    We want to create a REST API for DLRN, to allow different CI jobs
    to get a repository to test, and publish results on their CI tests.
    
    The API design is included in dlrn/doc/source/api.rst. To run the
    API server, use scripts/api.py and it will start a simple Flask web
    server.
    
    The API server can also be run under Apache, as a WSGI process. The
    steps to do so are detailed in dlrn/doc/source/api.rst.
    
    Change-Id: If2bdaf642d04e17aac80f0c61265213d020f75c3

---
+++ dlrn/api/__init__.py
@@ -0,0 +1,24 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object('dlrn.api.config')
+try:
+    app.config.from_pyfile(os.environ['CONFIG_FILE'], silent=True)
+except KeyError:
+    pass
+
+from dlrn.api import dlrn_api  # nopep8
