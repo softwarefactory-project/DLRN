ee20526c974640564600dcb708ceee32c42e98f7
Javier Pena <jpena@redhat.com>
2016-11-11 12:26:13 +0100

    Initial DLRN API
    We want to create a REST API for DLRN, to allow different CI jobs
    to get a repository to test, and publish results on their CI tests.
    
    The API design is included in dlrn/doc/source/api.rst. To run the
    API server, use scripts/api.py and it will start a simple Flask web
    server.
    
    The API server can also be run under Apache, as a WSGI process. The
    steps to do so are detailed in dlrn/doc/source/api.rst.
    
    Change-Id: If2bdaf642d04e17aac80f0c61265213d020f75c3

---
+++ dlrn/migrations/versions/638f980c9169_add_tables_required_by_dlrn_api.py
@@ -0,0 +1,54 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Add tables required by DLRN API
+
+Revision ID: 638f980c9169
+Revises: 1268c799620f
+Create Date: 2016-11-22 12:43:21.363066
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '638f980c9169'
+down_revision = '1268c799620f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('users',
+                    sa.Column('username', sa.String(256), nullable=False),
+                    sa.Column('password', sa.String(256), nullable=False),
+                    sa.PrimaryKeyConstraint('username'))
+
+    op.create_table('civotes',
+                    sa.Column('id', sa.Integer(), nullable=False),
+                    sa.Column('commit_id', sa.Integer(),
+                              sa.ForeignKey('commits.id'),
+                              nullable=False),
+                    sa.Column('ci_name', sa.String(256), nullable=True),
+                    sa.Column('ci_url', sa.String(1024), nullable=True),
+                    sa.Column('ci_vote', sa.Boolean(), nullable=True),
+                    sa.Column('ci_in_progress', sa.Boolean(), nullable=True),
+                    sa.Column('timestamp', sa.Integer(), nullable=True),
+                    sa.Column('notes', sa.Text(), nullable=True),
+                    sa.PrimaryKeyConstraint('id'))
+
+
+def downgrade():
+    op.drop_table('users')
+    op.drop_table('civotes')
