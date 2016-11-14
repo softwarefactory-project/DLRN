7106dd6a4da58b71737c868d1e85e2e4593a5e49
Javier Pena <jpena@redhat.com>
2016-11-14 16:02:38 +0100

    Move notification-related functions to separate module
    Continuing with the effort to reduce the shell.py size, let's move
    notification functions (submit_review and sendnotifymail) to a
    separate module.
    
    Change-Id: I2d1d56d21e20a19b20ca6c79010c20c2e715c5f8

--- dlrn/shell.py
+++ dlrn/shell.py
@@ -13,17 +13,13 @@
 
 from __future__ import print_function
 import argparse
-import copy
 import logging
 import multiprocessing
 import os
 import shutil
-import smtplib
 import sys
 
 from functools import partial
-
-from email.mime.text import MIMEText
 
 from prettytable import PrettyTable
 import sh
@@ -40,6 +36,8 @@ from dlrn.db import getLastBuiltCommit
 from dlrn.db import getLastProcessedCommit
 from dlrn.db import getSession
 from dlrn.db import Project
+from dlrn.notifications import sendnotifymail
+from dlrn.notifications import submit_review
 from dlrn.reporting import genreports
 from dlrn.reporting import get_commit_url
 from dlrn.rpmspecfile import RpmSpecCollection
@@ -54,24 +52,6 @@ logging.basicConfig(level=logging.ERROR,
                     format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
 logger = logging.getLogger("dlrn")
 logger.setLevel(logging.INFO)
-
-
-notification_email = """
-A build of the package %(name)s has failed against the current master[1] of
-the upstream project, please see log[2] and update the packaging[3].
-
-You are receiving this email because you are listed as one of the
-maintainers for the %(name)s package[4].
-
-If you have any questions please see the RDO master packaging guide[5] and
-feel free to ask questions on the RDO irc channel (#rdo on Freenode).
-
-[1] - %(upstream)s
-[2] - %(logurl)s
-[3] - %(master-distgit)s
-[4] - https://github.com/redhat-openstack/rdoinfo/blob/master/rdo.yml
-[5] - https://www.rdoproject.org/packaging/rdo-packaging.html#master-pkg-guide
-"""
 
 default_options = {'maxretries': '3', 'tags': None, 'gerrit': None,
                    'templatedir': os.path.join(
@@ -559,34 +539,6 @@ def compare():
     print(table)
 
 
-def sendnotifymail(packages, commit):
-    config_options = getConfigOptions()
-    error_details = copy.copy(
-        [package for package in packages
-            if package["name"] == commit.project_name][0])
-    error_details["logurl"] = "%s/%s" % (config_options.baseurl,
-                                         commit.getshardedcommitdir())
-    error_body = notification_email % error_details
-
-    msg = MIMEText(error_body)
-    msg['Subject'] = '[dlrn] %s master package build failed' % \
-                     commit.project_name
-
-    email_from = 'no-reply@delorean.com'
-    msg['From'] = email_from
-
-    email_to = error_details['maintainers']
-    msg['To'] = "packagers"
-
-    if config_options.smtpserver:
-        logger.info("Sending notify email to %r" % email_to)
-        s = smtplib.SMTP(config_options.smtpserver)
-        s.sendmail(email_from, email_to, msg.as_string())
-        s.quit()
-    else:
-        logger.info("Skipping notify email to %r" % email_to)
-
-
 def refreshrepo(url, path, branch="master", local=False):
     logger.info("Getting %s to %s (%s)" % (url, path, branch))
     checkout_not_present = not os.path.exists(path)
@@ -790,23 +742,3 @@ def sync_repo(commit):
         except Exception as e:
             logger.warn('Failed to rsync content to %s ,'
                         'got error %s' % (rsyncdest, e))
-
-
-def submit_review(commit, env_vars):
-    config_options = getConfigOptions()
-    datadir = os.path.realpath(config_options.datadir)
-    scriptsdir = os.path.realpath(config_options.scriptsdir)
-    yumrepodir = os.path.join("repos", commit.getshardedcommitdir())
-
-    project_name = commit.project_name
-
-    run_cmd = []
-    if env_vars:
-        for env_var in env_vars:
-            run_cmd.append(env_var)
-
-    run_cmd.extend([os.path.join(scriptsdir, "submit_review.sh"),
-                    project_name, os.path.join(datadir, yumrepodir),
-                    datadir, config_options.baseurl,
-                    os.path.realpath(commit.distgit_dir)])
-    sh.env(run_cmd)
