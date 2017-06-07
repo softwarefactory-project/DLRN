60389b7e184a1534fa293a3535c725535d8ebb59
Javier Pena <jpena@redhat.com>
2017-06-07 12:07:07 +0200

    Support distgits with spec in subdirectories in rdoinfo driver
    We want to be able to use some spec templates from the upstream
    rpm-packaging project in DLRN using the rdoinfo driver. That requires
    us to support a distgit where the spec is not located at the top
    level, but inside a subdirectory. This patch adds support for that,
    using a tag in rdoinfo for that (distgit-path). A potential rdoinfo
    configuration that would support this is:
    
    package-configs:
      rpm-packaging:
        name: '%(project)s'
        upstream: git://git.openstack.org/openstack/%(project)s
        distgit: https://github.com/openstack/rpm-packaging
        master-distgit: https://github.com/openstack/rpm-packaging
        patches: http://review.rdoproject.org/r/p/openstack/%(project)s.git
        review-patches: ssh://review.rdoproject.org:29418/puppet/%(project)s.git
        review-origin: ssh://review.rdoproject.org:29418/puppet/%(project)s-distgit.git
        distro-branch: master
        maintainers:
        - jpena@redhat.com
        tags:
          pike-uc:
            distro-branch: master
          pike:
            distro-branch: master
    
    ...
    
    - project: openstack-macros
      conf: rpm-packaging
      upstream: git://git.openstack.org/openstack/rpm-packaging
      distgit-path: /openstack/%(project)s
    
    Change-Id: I1ef7472b169351c86133c7f4e1960c2e2b0ce19d

--- dlrn/tests/test_repositories.py
+++ dlrn/tests/test_repositories.py
@@ -32,7 +32,7 @@ class TestRefreshRepo(base.TestCase):
                     mock.call(sh.git.fetch, 'origin'),
                     mock.call(sh.git.checkout, '-f', 'branch'),
                     mock.call(sh.git.reset, '--hard', 'origin/branch'),
-                    mock.call(sh.git.log, '--pretty=format:%H %ct', '-1')]
+                    mock.call(sh.git.log, '--pretty=format:%H %ct', '-1', '.')]
         self.assertEqual(sh_mock.call_args_list, expected)
 
     @mock.patch('os.path.exists', return_value=True)
@@ -44,13 +44,13 @@ class TestRefreshRepo(base.TestCase):
                     mock.call(sh.git.fetch, 'origin'),
                     mock.call(sh.git.checkout, '-f', 'branch'),
                     mock.call(sh.git.reset, '--hard', 'origin/branch'),
-                    mock.call(sh.git.log, '--pretty=format:%H %ct', '-1')]
+                    mock.call(sh.git.log, '--pretty=format:%H %ct', '-1', '.')]
         self.assertEqual(sh_mock.call_args_list, expected)
 
     @mock.patch('os.path.exists', return_value=True)
     def test_dont_fetch_if_local_repo_exists(self, path_mock, sh_mock):
         repositories.refreshrepo('url', 'path', branch='branch', local=True)
-        expected = [mock.call(sh.git.log, '--pretty=format:%H %ct', '-1')]
+        expected = [mock.call(sh.git.log, '--pretty=format:%H %ct', '-1', '.')]
         self.assertEqual(sh_mock.call_args_list, expected)
 
     def test_clone_fetch_if_local_repo_missing(self, sh_mock):
@@ -59,5 +59,5 @@ class TestRefreshRepo(base.TestCase):
                     mock.call(sh.git.fetch, 'origin'),
                     mock.call(sh.git.checkout, '-f', 'branch'),
                     mock.call(sh.git.reset, '--hard', 'origin/branch'),
-                    mock.call(sh.git.log, '--pretty=format:%H %ct', '-1')]
+                    mock.call(sh.git.log, '--pretty=format:%H %ct', '-1', '.')]
         self.assertEqual(sh_mock.call_args_list, expected)
