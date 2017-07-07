ec004f39e434b4adc5f813c6527d7a3f709160c8
Javier Pena <jpena@redhat.com>
2017-07-07 13:14:57 +0200

    Allow building multiple packages using dependent patches
    We want to allow run_tests.sh to build more than one package in the
    gate, if several patches are linked using Depends-On:. This requires
    some changes:
    
    - Update run_tests.sh to parse ZUUL_CHANGES and extract the projects
      to build from there
    - Make sure we use zuul-cloner to clone all repos
    - Run dlrn with all package names in a single command-line, so all
      are included in report.html
    - The above and the need to specify an order require us to use --order
      in the dlrn command line. That also needs a change in the way we
      compute the order, since we previously did it only when a full run
      was performed.
    
    Change-Id: Icafd405818b921a341f8e4581f1ad970784e074b

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,6 +6,11 @@ GIT_BASE_URL="https://review.rdoproject.org/r/p"
 OPENSTACK_GIT_URL="git://git.openstack.org"
 RDOINFO="${1:-$GIT_BASE_URL/rdoinfo}"
 
+function filterref(){
+    PROJ=${1%%:*}
+    echo $PROJ
+}
+
 # Display the current commit
 git log -1
 
@@ -23,9 +28,19 @@ PROJECT_DISTRO_BRANCH="rpm-master"
 # If we're actually building for a Zuul project, build that unless it's DLRN
 [[ ! -z "${ZUUL_PROJECT}" && "${ZUUL_PROJECT}" != "DLRN" ]] && PROJECT_DISTRO="${ZUUL_PROJECT}"
 
-# The rdoinfo project name isn't prefixed by openstack/ or suffixed by -distgit
-PROJECT_TO_BUILD=${PROJECT_DISTRO#*/}
-PROJECT_TO_BUILD=$(sed "s/-distgit//" <<< "${PROJECT_TO_BUILD}")
+PROJECTS_TO_BUILD=""
+if [[ ! -z "${ZUUL_CHANGES}" ]]; then
+  ZUUL_CHANGES=${ZUUL_CHANGES//^/ }
+  for CHANGE in $ZUUL_CHANGES; do
+    PROJ=$(filterref $CHANGE)
+    # There may be non-distgit dependent changes, e.g. rdoinfo
+    if [[ "${PROJ}" =~ '-distgit' ]]; then
+      PROJECTS_TO_BUILD="$PROJECTS_TO_BUILD $PROJ"
+    fi
+  done
+else
+  PROJECTS_TO_BUILD=${PROJECT_DISTRO}
+fi
 
 # Fetch rdoinfo using zuul_cloner, if available
 if type -p zuul-cloner; then
@@ -34,10 +49,6 @@ else
     rm -rf /tmp/rdoinfo
     git clone ${RDOINFO} /tmp/rdoinfo
 fi
-
-# Map to rdoinfo
-PROJECT_TO_BUILD_MAPPED=$(rdopkg findpkg $PROJECT_TO_BUILD -l /tmp/rdoinfo | grep ^name | awk '{print $2}')
-PROJECT_DISTRO_DIR=${PROJECT_TO_BUILD_MAPPED}_distro
 
 # Prepare config
 target="centos"
@@ -76,62 +87,6 @@ sed -i "s%tags=.*%tags=${branch}%" projects.ini
 # Prepare directories for distro repo
 mkdir -p data/repos
 
-# Remove distro dir first for idempotency
-rm -rf data/$PROJECT_DISTRO_DIR
-
-# Clone distro repo
-if type -p zuul-cloner; then
-    zuul-cloner --workspace data/ $GIT_BASE_URL $PROJECT_DISTRO --branch $PROJECT_DISTRO_BRANCH
-    mv data/$PROJECT_DISTRO data/$PROJECT_DISTRO_DIR
-else
-    # We're outside the gate, just do a regular git clone
-    pushd data/
-    git clone "${GIT_BASE_URL}/${PROJECT_DISTRO}" $PROJECT_DISTRO_DIR
-    cd $PROJECT_DISTRO_DIR
-    git checkout $PROJECT_DISTRO_BRANCH
-    popd
-fi
-
-# Lookup if we need to extract an upstream review
-pushd data/${PROJECT_DISTRO_DIR}
-[ -n "$UPSTREAM_ID" ] || UPSTREAM_ID=$(git log -1|sed -ne 's/\s*Upstream-Id: //p')
-git log -1
-popd
-
-if [ -n "$UPSTREAM_ID" ]; then
-    # Get upstream URL
-    UPSTREAM_URL=$(rdopkg findpkg $PROJECT_TO_BUILD -l /tmp/rdoinfo | grep ^upstream | awk '{print $2}')
-    UPSTREAM_PROJECT_NAME=$(echo ${UPSTREAM_URL} | awk -F/ '{print $NF}')
-    rm -rf data/${PROJECT_TO_BUILD_MAPPED}
-    if type -p zuul-cloner; then
-        # Only build in the check pipeline to avoid merging a change
-        # in packaging that is dependent of an non merged upstream
-        # change
-        if [ "${ZUUL_PIPELINE}" = "check" ]; then
-            zuul-cloner --workspace data/ $OPENSTACK_GIT_URL openstack/${UPSTREAM_PROJECT_NAME}
-            mv data/openstack/${UPSTREAM_PROJECT_NAME} data/${PROJECT_TO_BUILD_MAPPED}
-        else
-            NOT_EXTRACTED=1
-        fi
-     else
-        git clone ${UPSTREAM_URL} "data/${PROJECT_TO_BUILD_MAPPED}"
-    fi
-    if [ -z "$NOT_EXTRACTED" ]; then
-        # We cannot run git review -d because we don't have an
-        # available account. So we do the same using curl, jq and git.
-        JSON=$(curl -s -L https://review.openstack.org/changes/$UPSTREAM_ID/revisions/current/review|sed 1d)
-        COMMIT=$(python -c 'import json;import sys; s = json.loads(sys.stdin.read(-1)); print s["revisions"].keys()[0]' <<< $JSON)
-        REF=$(python -c "import json;import sys; s = json.loads(sys.stdin.read(-1)); print s['revisions']['$COMMIT']['ref']" <<< $JSON)
-        pushd data/${PROJECT_TO_BUILD_MAPPED}
-        if [ -n "$REF" -a "$REF" != null ]; then
-            git fetch ${UPSTREAM_URL} $REF
-            git checkout FETCH_HEAD
-        fi
-        git log -1
-        popd
-    fi
-fi
-
 # If the commands below throws an error we still want the logs
 function copy_logs() {
     mkdir -p logs
@@ -140,8 +95,77 @@ function copy_logs() {
 }
 trap copy_logs ERR EXIT
 
+# Build all projects, hopefully the order given by Zuul was correct
+PACKAGE_BUILD_LIST=""
+for PROJECT_TO_BUILD in ${PROJECTS_TO_BUILD}; do
+    PROJECT_DISTRO=$PROJECT_TO_BUILD
+    # The rdoinfo project name isn't prefixed by openstack/ or suffixed by -distgit
+    PROJECT_TO_BUILD=${PROJECT_DISTRO#*/}
+    PROJECT_TO_BUILD=$(sed "s/-distgit//" <<< "${PROJECT_TO_BUILD}")
+    # Map to rdoinfo
+    PROJECT_TO_BUILD_MAPPED=$(rdopkg findpkg $PROJECT_TO_BUILD -l /tmp/rdoinfo | grep ^name | awk '{print $2}')
+    PROJECT_DISTRO_DIR=${PROJECT_TO_BUILD_MAPPED}_distro
+
+    # Remove distro dir first for idempotency
+    rm -rf data/$PROJECT_DISTRO_DIR
+
+    # Clone distro repo
+    if type -p zuul-cloner; then
+        zuul-cloner --workspace data/ $GIT_BASE_URL $PROJECT_DISTRO --branch $PROJECT_DISTRO_BRANCH
+        mv data/$PROJECT_DISTRO data/$PROJECT_DISTRO_DIR
+    else
+        # We're outside the gate, just do a regular git clone
+        pushd data/
+        git clone "${GIT_BASE_URL}/${PROJECT_DISTRO}" $PROJECT_DISTRO_DIR
+        cd $PROJECT_DISTRO_DIR
+        git checkout $PROJECT_DISTRO_BRANCH
+        popd
+    fi
+
+    # Lookup if we need to extract an upstream review
+    pushd data/${PROJECT_DISTRO_DIR}
+    [ -n "$UPSTREAM_ID" ] || UPSTREAM_ID=$(git log -1|sed -ne 's/\s*Upstream-Id: //p')
+    git log -1
+    popd
+
+    if [ -n "$UPSTREAM_ID" ]; then
+        # Get upstream URL
+        UPSTREAM_URL=$(rdopkg findpkg $PROJECT_TO_BUILD -l /tmp/rdoinfo | grep ^upstream | awk '{print $2}')
+        UPSTREAM_PROJECT_NAME=$(echo ${UPSTREAM_URL} | awk -F/ '{print $NF}')
+        rm -rf data/${PROJECT_TO_BUILD_MAPPED}
+        if type -p zuul-cloner; then
+            # Only build in the check pipeline to avoid merging a change
+            # in packaging that is dependent of an non merged upstream
+            # change
+            if [ "${ZUUL_PIPELINE}" = "check" ]; then
+                zuul-cloner --workspace data/ $OPENSTACK_GIT_URL openstack/${UPSTREAM_PROJECT_NAME}
+                mv data/openstack/${UPSTREAM_PROJECT_NAME} data/${PROJECT_TO_BUILD_MAPPED}
+            else
+                NOT_EXTRACTED=1
+            fi
+         else
+            git clone ${UPSTREAM_URL} "data/${PROJECT_TO_BUILD_MAPPED}"
+        fi
+        if [ -z "$NOT_EXTRACTED" ]; then
+            # We cannot run git review -d because we don't have an
+            # available account. So we do the same using curl, jq and git.
+            JSON=$(curl -s -L https://review.openstack.org/changes/$UPSTREAM_ID/revisions/current/review|sed 1d)
+            COMMIT=$(python -c 'import json;import sys; s = json.loads(sys.stdin.read(-1)); print s["revisions"].keys()[0]' <<< $JSON)
+            REF=$(python -c "import json;import sys; s = json.loads(sys.stdin.read(-1)); print s['revisions']['$COMMIT']['ref']" <<< $JSON)
+            pushd data/${PROJECT_TO_BUILD_MAPPED}
+            if [ -n "$REF" -a "$REF" != null ]; then
+                git fetch ${UPSTREAM_URL} $REF
+                git checkout FETCH_HEAD
+            fi
+            git log -1
+            popd
+        fi
+    fi
+    PACKAGE_BUILD_LIST="$PACKAGE_BUILD_LIST --package-name $PROJECT_TO_BUILD_MAPPED"
+done
+
 # Run DLRN
-dlrn --head-only --package-name $PROJECT_TO_BUILD_MAPPED --dev --local --info-repo /tmp/rdoinfo --verbose-mock
+dlrn --head-only $PACKAGE_BUILD_LIST --dev --local --info-repo /tmp/rdoinfo --verbose-mock --order
 copy_logs
 # Clean up mock cache, just in case there is a change for the next run
 mock -r data/dlrn-1.cfg --scrub=all
