0126a57d8613711606c2b5c88c6a7a87851859a2
Fabien Boucher <fboucher@redhat.com>
2018-02-27 15:36:54 +0000

    Change column max size users tables
    It looks like DLRN cannot create the users tables
    on MariaDB due to the size of the username field.
    It seems to be related to the use of username as
    primary key that exceed the 767 Bytes limitation.
    
    sqlalchemy.exc.InternalError: (pymysql.err.InternalError) (1071, u'Specified key was too long; max key length is 767 bytes') [SQL: u'\nCREATE TABLE users (\n\tusername VARCHAR(256) NOT NULL,
    \n\tpassword VARCHAR(256) NOT NULL, \n\tPRIMARY KEY (username)\n)\n\n'] (Background on this error at: http://sqlalche.me/e/2j85)
    
    The database where I get the error is created with
    these options "CHARACTER SET utf8 COLLATE utf8_general_ci;"
    
    It seems to be a common issue:
    https://stackoverflow.com/questions/1814532/1071-specified-key-was-too-long-max-key-length-is-767-bytes
    
    When username VARCHAR length is set to 255 instead of 256 it
    solves the issue.
    
    Change-Id: I87d929030744f19132282e132b58aa13c4f750e5

---
+++ dlrn/migrations/versions/2a0313a8a7d6_change_user_usernames_column_length.py
@@ -0,0 +1,88 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""change user/usernames column length
+
+Revision ID: 2a0313a8a7d6
+Revises: cab7697f6564
+Create Date: 2018-02-27 15:35:04.667089
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '2a0313a8a7d6'
+down_revision = 'cab7697f6564'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table("civotes") as batch_op:
+        batch_op.drop_constraint('civ_user_fk', type_='foreignkey')
+
+    with op.batch_alter_table("promotions") as batch_op:
+        batch_op.drop_constraint('prom_user_fk', type_='foreignkey')
+
+    with op.batch_alter_table("users") as batch_op:
+        batch_op.alter_column('username', existing_type=sa.String(256),
+                              type_=sa.String(255))
+
+    with op.batch_alter_table("civotes") as batch_op:
+        batch_op.alter_column('user', existing_type=sa.String(256),
+                              type_=sa.String(255))
+        batch_op.create_foreign_key(
+            constraint_name="civ_user_fk",
+            referent_table="users",
+            local_cols=["user"],
+            remote_cols=["username"])
+
+    with op.batch_alter_table("promotions") as batch_op:
+        batch_op.alter_column('user', existing_type=sa.String(256),
+                              type_=sa.String(255))
+        batch_op.create_foreign_key(
+            constraint_name="prom_user_fk",
+            referent_table="users",
+            local_cols=["user"],
+            remote_cols=["username"])
+
+
+def downgrade():
+    with op.batch_alter_table("civotes") as batch_op:
+        batch_op.drop_constraint('civ_user_fk', type_='foreignkey')
+
+    with op.batch_alter_table("promotions") as batch_op:
+        batch_op.drop_constraint('prom_user_fk', type_='foreignkey')
+
+    with op.batch_alter_table("users") as batch_op:
+        batch_op.alter_column('username', existing_type=sa.String(255),
+                              type_=sa.String(256))
+
+    with op.batch_alter_table("civotes") as batch_op:
+        batch_op.alter_column('user', existing_type=sa.String(255),
+                              type_=sa.String(256))
+        batch_op.create_foreign_key(
+            constraint_name="civ_user_fk",
+            referent_table="users",
+            local_cols=["user"],
+            remote_cols=["username"])
+
+    with op.batch_alter_table("promotions") as batch_op:
+        batch_op.alter_column('user', existing_type=sa.String(255),
+                              type_=sa.String(256))
+        batch_op.create_foreign_key(
+            constraint_name="prom_user_fk",
+            referent_table="users",
+            local_cols=["user"],
+            remote_cols=["username"])
