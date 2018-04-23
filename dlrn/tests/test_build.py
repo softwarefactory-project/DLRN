e40c9f36e32a6b7345148bf876a9cc86e01baf13
Javier Pena <jpena@redhat.com>
2018-04-23 17:20:20 +0200

    Modular rpm build drivers for DLRN
    Currently, DLRN supports building RPMs using Mock (default option)
    and Copr. The way it is done is a bit hacky, via some checks in the
    build_rpm.sh script.
    
    This patch aims at changing that, by creating modular drivers to
    build packages. The initial patch adds support for Mock, with Copr
    coming down the road in a follow-up patch.
    
    TG-1229
    
    Change-Id: Ic02f2578f5075dedcd83d953c44c665ee2a68271

--- dlrn/tests/test_build.py
+++ dlrn/tests/test_build.py
@@ -39,6 +39,12 @@ class FakePkgInfo(object):
 dlrn.shell.pkginfo = FakePkgInfo()
 
 
+def mocked_listdir(directory):
+    return ['python-pysaml2-3.0-1a.el7.centos.src.rpm']
+
+
+@mock.patch('sh.restorecon', create=True)
+@mock.patch('sh.env', create=True)
 @mock.patch.object(sh.Command, '__call__', autospec=True)
 class TestBuild(base.TestCase):
     def setUp(self):
@@ -65,15 +71,23 @@ class TestBuild(base.TestCase):
         shutil.rmtree(self.config.scriptsdir)
         os.close(self.db_fd)
 
-    def test_build_rpm_wrapper(self, sh_mock):
+    @mock.patch('os.listdir', side_effect=mocked_listdir)
+    def test_build_rpm_wrapper(self, ld_mock, sh_mock, env_mock, rc_mock):
         commit = db.getCommits(self.session)[-1]
         build_rpm_wrapper(commit, False, False, False, None, True)
-        # git and build_rpms has been called
-        self.assertEqual(sh_mock.call_count, 2)
+        # 4 sh calls:
+        # 1- git reset --hard
+        # 2- build_srpm.sh
+        # 3- mock (handled by env_mock)
+        # 4- restorecon (handled by rc_mock)
+        self.assertEqual(env_mock.call_count, 2)
+        self.assertEqual(rc_mock.call_count, 1)
+        self.assertEqual(sh_mock.call_count, 1)
         self.assertTrue(os.path.exists(os.path.join(self.config.datadir,
                                                     "dlrn-1.cfg")))
 
-    def test_build(self, sh_mock):
+    @mock.patch('os.listdir', side_effect=mocked_listdir)
+    def test_build(self, ld_mock, sh_mock, env_mock, rc_mock):
         commit = db.getCommits(self.session)[-1]
         try:
             build(None, commit, None, False, False, False, True)
---
+++ dlrn/tests/test_driver_mock.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+import os
+import shutil
+import tempfile
+
+from dlrn.config import ConfigOptions
+from dlrn.drivers.mockdriver import MockBuildDriver
+from dlrn.shell import default_options
+from dlrn.tests import base
+from six.moves import configparser
+
+
+def _mocked_listdir(directory):
+    return ['python-pysaml2-3.0-1a.el7.centos.src.rpm']
+
+
+@mock.patch('sh.restorecon', create=True)
+@mock.patch('sh.env', create=True)
+@mock.patch('os.listdir', side_effect=_mocked_listdir)
+class TestDriverMock(base.TestCase):
+    def setUp(self):
+        super(TestDriverMock, self).setUp()
+        config = configparser.RawConfigParser(default_options)
+        config.read("projects.ini")
+        self.config = ConfigOptions(config)
+        self.temp_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        super(TestDriverMock, self).tearDown()
+        shutil.rmtree(self.temp_dir)
+
+    def test_build_package(self, ld_mock, env_mock, rc_mock):
+        os.environ['MOCK_CONFIG'] = 'dlrn-1.cfg'
+        driver = MockBuildDriver(cfg_options=self.config)
+        driver.build_package(output_directory=self.temp_dir)
+
+        datadir = os.path.realpath(self.config.datadir)
+        expected = [mock.call('/usr/bin/mock', '-v', '-r',
+                              '%s/dlrn-1.cfg' % datadir,
+                              '--resultdir', self.temp_dir,
+                              '--rebuild',
+                              '%s/python-pysaml2-3.0-1a.el7.centos.src.rpm' %
+                              self.temp_dir,
+                              _err=driver._process_mock_output,
+                              _out=driver._process_mock_output,
+                              postinstall=True)]
+        # 2 sh calls:
+        # 1- mock (handled by env_mock)
+        # 2- restorecon (handled by rc_mock)
+        self.assertEqual(env_mock.call_count, 1)
+        self.assertEqual(rc_mock.call_count, 1)
+        self.assertEqual(env_mock.call_args_list, expected)
+
+    def test_build_package_noinstall(self, ld_mock, env_mock, rc_mock):
+        os.environ['MOCK_CONFIG'] = 'dlrn-1.cfg'
+        config_options = self.config
+        config_options.install_after_build = False
+        driver = MockBuildDriver(cfg_options=config_options)
+        driver.build_package(output_directory=self.temp_dir)
+
+        datadir = os.path.realpath(config_options.datadir)
+        expected = [mock.call('/usr/bin/mock', '-v', '-r',
+                              '%s/dlrn-1.cfg' % datadir,
+                              '--resultdir', self.temp_dir,
+                              '--rebuild',
+                              '%s/python-pysaml2-3.0-1a.el7.centos.src.rpm' %
+                              self.temp_dir,
+                              _err=driver._process_mock_output,
+                              _out=driver._process_mock_output,
+                              postinstall=False)]
+        # 2 sh calls:
+        # 1- mock (handled by env_mock)
+        # 2- restorecon (handled by rc_mock)
+        self.assertEqual(env_mock.call_count, 1)
+        self.assertEqual(rc_mock.call_count, 1)
+        self.assertEqual(env_mock.call_args_list, expected)
+
+    def test_build_additional_mock_options(self, ld_mock, env_mock, rc_mock):
+        os.environ['MOCK_CONFIG'] = 'dlrn-1.cfg'
+        driver = MockBuildDriver(cfg_options=self.config)
+        driver.build_package(output_directory=self.temp_dir,
+                             additional_mock_opts='-D repo_bootstrap 1')
+
+        datadir = os.path.realpath(self.config.datadir)
+        expected = [mock.call('/usr/bin/mock', '-v', '-r',
+                              '%s/dlrn-1.cfg' % datadir,
+                              '--resultdir', self.temp_dir,
+                              '-D repo_bootstrap 1',
+                              '--rebuild',
+                              '%s/python-pysaml2-3.0-1a.el7.centos.src.rpm' %
+                              self.temp_dir,
+                              _err=driver._process_mock_output,
+                              _out=driver._process_mock_output,
+                              postinstall=True)]
+        # 2 sh calls:
+        # 1- mock (handled by mock_mock)
+        # 2- restorecon (handled by rc_mock)
+        self.assertEqual(env_mock.call_count, 1)
+        self.assertEqual(rc_mock.call_count, 1)
+        self.assertEqual(env_mock.call_args_list, expected)
