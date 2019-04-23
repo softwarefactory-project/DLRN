3ed59335cac87d312c638edb89d056ae120ed0ca
Fabien Boucher <fboucher@redhat.com>
2019-04-23 17:46:17 +0200

    Add the local driver. usage: distgit classic build
    This patch brings a simple solution to build packages from
    a local directory containing a spec file. The spec file
    must be the final spec because no processing will be perform
    by driver.
    
    No git repositories, neither distroinfo repo needed.
    
    Usage Example:
    projects.ini: pkginfo_driver=dlrn.drivers.local.LocalDriver
    
    cd python-redis;
    ls python-redis.spec  sources
    dlrn --config-file ../projects.ini
    2019-05-06 14:09:43,507 INFO:dlrn-local-driver:Copy distgit source from /home/fabien/dlrn-exp/python-redis to /home/fabien/dlrn-exp/data/python-redis
    2019-05-06 14:09:43,508 INFO:dlrn-local-driver:Got version 3.1.0 for python-redis from the spec
    2019-05-06 14:09:43,638 INFO:dlrn-build:Processing python-redis python-redis-3.1.0
    ls -al ../data/repos/py/th/python-redis-3.1.0_69fd12b5/*.noarch.rpm
    -rw-r--r--. 1 fabien mock 124612  6 mai   14:11 ../data/repos/py/th/python-redis-3.1.0_69fd12b5/python2-redis-3.1.0-1.fc31.noarch.rpm
    -rw-r--r--. 1 fabien mock 125008  6 mai   14:11 ../data/repos/py/th/python-redis-3.1.0_69fd12b5/python3-redis-3.1.0-1.fc31.noarch.rpm
    
    Change-Id: I494b5e32e113727a9f429cde5eebf31f7b472c7c

---
+++ dlrn/drivers/local.py
@@ -0,0 +1,111 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# The LocalDriver provides the following:
+#
+# 1- A getpackages function based on finding all directories inside a specific
+#    git repo, where each directory represents a package
+#
+# 2- A getinfo function based on a single-distgit repo paradigm
+
+import logging
+import os
+import sh
+import shutil
+
+from dlrn.db import Commit
+from dlrn.drivers.pkginfo import PkgInfoDriver
+from rdopkg.utils import specfile
+
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger("dlrn-local-driver")
+logger.setLevel(logging.INFO)
+
+
+class LocalDriver(PkgInfoDriver):
+    DRIVER_CONFIG = {
+        'local_driver': {
+        }
+    }
+
+    def _get_infos_from_pkg(self, packagepath):
+        version = None
+        package = None
+        if not os.path.isdir(os.path.join(packagepath, '.git')):
+            raise RuntimeError(
+                'The distgit directory must a be git repository')
+        for pkgfile in os.listdir(packagepath):
+            if pkgfile.endswith('.spec'):
+                spec = specfile.Spec(fn=os.path.join(packagepath, pkgfile))
+                version = spec.get_tag('Version')
+                package = pkgfile.replace('.spec', '')
+        if not version:
+            raise RuntimeError("No spec file or no version found")
+        return package, version
+
+    def getpackages(self, **kwargs):
+        datadir = self.config_options.datadir
+        # src_dir is only set for the test case. Normal behavior is
+        # to look at the current directory.
+        if 'src_dir' in kwargs:
+            src_dir = kwargs['src_dir']
+        else:
+            src_dir = os.getcwd()
+        is_src_dir_distgit = [
+            f for f in os.listdir(src_dir) if f.endswith('.spec')]
+        if not is_src_dir_distgit:
+            return []
+        package, version = self._get_infos_from_pkg(src_dir)
+        dest_dir = os.path.join(datadir, package)
+        logger.info("Copy distgit source from %s to %s" % (src_dir, dest_dir))
+        if os.path.isdir(dest_dir):
+            shutil.rmtree(dest_dir)
+        shutil.copytree(src_dir, dest_dir)
+        pkg_hash = {}
+        pkg_hash['name'] = package
+        pkg_hash['maintainers'] = 'test@example.com'
+        pkg_hash['master-distgit'] = dest_dir
+        pkg_hash['upstream'] = 'Unknown'
+        logger.info(
+            "Got version %s for %s from the spec" % (version, package))
+        pkg_hash['source-branch'] = version
+        return [pkg_hash]
+
+    def getinfo(self, **kwargs):
+        project = kwargs.get('project')
+        package = kwargs.get('package')
+        datadir = self.config_options.datadir
+        distro_dir = os.path.join(datadir, package['name'])
+
+        # Get distro_hash from last commit in distgit directory
+        git = sh.git.bake(_cwd=package['master-distgit'], _tty_out=False)
+        repoinfo = str(git.log("--pretty=format:%H %ct", "-1", ".")
+                       ).strip().split(" ")
+        distro_hash = repoinfo[0]
+        dt_distro = repoinfo[1]
+
+        project_toprocess = []
+        commit_hash = "%s-%s" % (project, package['source-branch'])
+        commit = Commit(
+            dt_commit=0, project_name=project,
+            type='rpm',
+            commit_hash=commit_hash, repo_dir=None,
+            distro_hash=distro_hash, dt_distro=dt_distro,
+            distgit_dir=distro_dir,
+            commit_branch=package['source-branch'],
+            dt_extended=0, extended_hash=None)
+        project_toprocess.append(commit)
+
+        return project_toprocess
+
+    def preprocess(self, **kwargs):
+        pass
