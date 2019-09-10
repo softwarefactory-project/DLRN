3c154bde9aba641a6ca2c5ab070433edee7a1cc0
Javier Pena <jpena@redhat.com>
2019-09-10 17:38:17 +0200

    Provide a container image for the DLRN API
    We want to provide a ready-made container image for the DLRN API,
    so it can be used to test other software easily. This commit provides
    a Dockerfile and basic scripts to run it. It also creates the required
    file to build and distribute the container in the CentOS Container
    Registry [1].
    
    [1] - https://registry.centos.org
    
    TG-2858
    
    Change-Id: I960ead055dd220613f27c433ea8064101b26c741

---
+++ contrib/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+if [ -n "$DLRNAPI_USE_SAMPLE_DATA" ];
+then
+    python /import.py
+    mkdir -p /data/repos/17/23/17234e9ab9dfab4cf5600f67f1d24db5064f1025_024e24f0
+    mkdir -p /data/repos/1c/67/1c67b1ab8c6fe273d4e175a14f0df5d3cbbd0edc_8170b868
+    mkdir -p /data/repos/1c/67/1c67b1ab8c6fe273d4e175a14f0df5d3cbbd0e77_8170b868
+fi
+
+python /DLRN/scripts/api.py
