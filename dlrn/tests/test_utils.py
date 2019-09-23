cde2fb024764b78be1a341045afaa86c0cc965a7
Javier Pena <jpena@redhat.com>
2019-09-23 10:40:17 +0200

    [WIP] Add component support to DLRN
    We want to introduce a new concept for DLRN builders, support for
    components. This means that a builder will be able to create packages
    for a single entity comprised of multiple sub-entities, each of which
    will have separate current/ and consistent/ symlinks. In the RDO world,
    the single entity would be "RDO", while each component could be
    "networking", "storage", "compute", etc.
    
    This commit adds support for this concept, initially with support only
    for the rdoinfo driver. It relies on the component definition described
    in [1], to assign a component to each package.
    
    Note that this changes the URLs for the hashed repositories, although
    compatibility is provided by having current/*.repo and consistent/*.repo
    top-level repo files, aggregating the .repo files from all components.
    
    Also note that DLRN API support is not included with this patch, it will
    be provided as a follow-up.
    
    [1] - https://review.rdoproject.org/r/22394
    
    Depends-On: https://review.rdoproject.org/r/22394
    Change-Id: Id2c7d6afdb398b19fe6ba48eeaa18db860b5d66d

--- dlrn/tests/test_utils.py
+++ dlrn/tests/test_utils.py
@@ -35,22 +35,25 @@ class Testdumpshas2file(TestsWithData):
 
         mock_fp = MagicMock()
         utils.dumpshas2file(mock_fp, commit, "a", "b", commit.status, 0,
+                            None,
                             ['python-saml2-1.0-1.el7.src.rpm'])
         utils.dumpshas2file(mock_fp, commit2, "a", "b", commit2.status, 1,
+                            'common',
                             ['python-alembic-1.0-2.el7.src.rpm'])
         utils.dumpshas2file(mock_fp, commit3, "a", "b", commit3.status, 2,
+                            'common',
                             ['file1-1.2-3.el7.noarch.rpm',
                              'file2-1.2-3.el7.src.rpm'])
         expected = [
             call.write(u'python-pysaml2,a,3a9326f251b9a4162eb0dfa9f1c924ef47c'
                        '2c55a,b,024e24f0cf4366c2290c22f24e42de714d1addd1'
-                       ',SUCCESS,0,python-saml2-1.0-1.el7\n'),
+                       ',SUCCESS,0,None,python-saml2-1.0-1.el7\n'),
             call.write(u'python-alembic,a,459549c9ab7fef91b2dc8986bc0643bb2f6'
                        'ec0c8,b,885e80778edb6cbb8ee4d8909623be8062369a04'
-                       ',FAILED,1,python-alembic-1.0-2.el7\n'),
+                       ',FAILED,1,common,python-alembic-1.0-2.el7\n'),
             call.write(u'python-tripleoclient,a,1da7b10e55abf8c518e8f61ee7966'
                        '188f0405f59,b,0b1ce934e5b2e7d45a448f6555d24036f9aeca51'
-                       ',SUCCESS,2,file2-1.2-3.el7\n')
+                       ',SUCCESS,2,common,file2-1.2-3.el7\n')
         ]
         self.assertEqual(mock_fp.mock_calls, expected)
 
